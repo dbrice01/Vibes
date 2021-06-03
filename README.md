5cc6edec83be8fc40b8fd5359629b7d378b8a0d4
dbrice01 <dbrice01@qub.ac.uk>
2021-06-03 15:44:20 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Vibes
+
+Developed with Unreal Engine 4
---
+++ Source/Vibes/VibesGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "VibesGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class VIBES_API AVibesGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
